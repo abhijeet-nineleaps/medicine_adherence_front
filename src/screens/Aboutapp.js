749ce383aed8771de1948f9f995560558d52ac6e
abhijeet-nineleaps <99660322+abhijeet-nineleaps@users.noreply.github.com>
2022-04-11 11:48:09 +0530

    Update Aboutapp.js

--- src/screens/Aboutapp.js
+++ src/screens/Aboutapp.js
@@ -1,16 +1,35 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import React from 'react';
 
 const About = () => {
+  const {height} = Dimensions.get('screen');
+  const {width} = Dimensions.get('screen');
+  const height_logo = height * 0.35;
+  const width_logo = width * 1;
+
   return (
-    <View
-      style={{alignItems: 'center', backgroundColor: 'white', height: '100%'}}>
-      <Text
-        style={{marginTop: 5, fontSize: 18, fontWeight: 'bold', color: 'gray'}}>
-        Medicine Adherence app which allows user to use medicine, reminder,
-        caretaker, patient, report and more features and never skip their
-        medications.
-      </Text>
+    <View style={{flex: 1}}>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          style={{height: height_logo, width: width_logo}}
+          source={require('../../assests/Medstick_1.png')}
+          resizeMode="stretch"
+        />
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <Text
+          style={{
+            marginTop: 30,
+            paddingLeft: 4,
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: 'gray',
+          }}>
+          Medicine Adherence app which allows user to use medicine, reminder,
+          caretaker, patient, report and more features and never skip their
+          medications.
+        </Text>
+      </View>
     </View>
   );
 };
