b9980d7151a836a55c45267e4246842bb9474293
Abhijeet Chhimwal <abhijeet.chhimwal@nineleaps.com>
2022-04-13 21:01:05 +0530

    About updated

--- src/screens/Aboutapp.js
+++ src/screens/Aboutapp.js
@@ -2,29 +2,16 @@ import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import React from 'react';
 
 const About = () => {
-  const {height} = Dimensions.get('screen');
-  const {width} = Dimensions.get('screen');
-  const height_logo = height * 0.35;
-  const width_logo = width * 1;
-
   return (
-    <View style={{flex: 1}}>
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
         <Image
-          style={{height: height_logo, width: width_logo}}
+          style={styles.image}
           source={require('../../assests/Medstick_1.png')}
-          resizeMode="stretch"
         />
       </View>
-      <View style={{alignItems: 'center'}}>
-        <Text
-          style={{
-            marginTop: 30,
-            paddingLeft: 4,
-            fontSize: 20,
-            fontWeight: 'bold',
-            color: 'gray',
-          }}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
           Medicine Adherence app which allows user to use medicine, reminder,
           caretaker, patient, report and more features and never skip their
           medications.
@@ -35,3 +22,32 @@ const About = () => {
 };
 
 export default About;
+
+const {height} = Dimensions.get('screen');
+const {width} = Dimensions.get('screen');
+const height_logo = height * 0.35;
+const width_logo = width * 1;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: height_logo,
+    width: width_logo,
+  },
+  textContainer: {
+    alignItems: 'center',
+  },
+  text: {
+    marginTop: 30,
+    paddingLeft: 4,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+});
