ef39b9d47773003b5285d2a0c39a159491d65cdf
Abhijeet Chhimwal <abhijeet.chhimwal@nineleaps.com>
2022-07-28 18:28:08 +0530

    code coverage improved

--- src/repositories/database/globalDb.tsx
+++ src/repositories/database/globalDb.tsx
@@ -1,9 +1,9 @@
 import SQLite from 'react-native-sqlite-storage';
+SQLite.enablePromise(true);
 function globalDb() {
  return SQLite.openDatabase({
     name: 'MedStickdb',
     location: 'default',
   });
 }
-
 export default globalDb;
