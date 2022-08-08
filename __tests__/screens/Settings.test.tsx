4d72803ca96319985e5036af459a90290688b67f
Abhijeet Chhimwal <abhijeet.chhimwal@nineleaps.com>
2022-08-08 17:28:36 +0530

    testing

--- __tests__/screens/Settings.test.tsx
+++ __tests__/screens/Settings.test.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import {create} from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
 import Settings from '../../src/screens/Settings';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+Enzyme.configure({adapter: new Adapter()});
 
-describe('Settins Screen', () => {
+describe('Settings Screen', () => {
   it('renders correctly', () => {
-    const tree = create(<Settings navigation={undefined}/>);
+    const tree = renderer.create(<Settings navigation={undefined} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  
+  it.only('test share button', () => {
+   const mockFn = jest.fn();
+  const wrapper = shallow(<Settings shareFnc={mockFn} />);
+  wrapper.find("#share").simulate("press");
+  });
+  it.only('test about button', () => {
+    const mockFn = jest.fn();
+   const wrapper = shallow(<Settings aboutFnc={mockFn} />);
+   wrapper.find("#about").simulate("press");
+   });
+   it.only('test open setting button', () => {
+    const mockFn = jest.fn();
+   const wrapper = shallow(<Settings openSetFnc={mockFn} />);
+   wrapper.find("#openSetting").simulate("press");
+   });
 });
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,30 +1,42 @@
+import {Linking, Share, View} from 'react-native';
 import React from 'react';
-import { Linking, Share, View} from 'react-native';
 import SettingsList from 'react-native-settings-list';
-import styles from "./screenStyles/SettingStyles";
-
-
+import styles from './screenStyles/SettingStyles';
 interface Props {
   navigation: any;
 }
-
 const Settings: React.FC<Props> = ({navigation}: Props) => {
+  const shareFnc = () => {
+    try {
+      Share.share({
+        title: 'Medstick',
+        message:
+          'Hello you are invited to use Medstick ' +
+          'https://play.google.com/store/apps/details?id=com.animesafar.dinterviewkit',
+        url: 'https://cdn.discordapp.com/attachments/941592669933682699/955175698568462437/vinaylogo.png',
+      });
+    } catch (error) {}
+  };
+  const aboutFnc = () => {
+    navigation.navigate('About');
+  };
+  const openSetFnc = () => {
+    Linking.openSettings();
+  };
   return (
     <View style={styles.container}>
       <SettingsList borderColor="white" backgroundColor="white">
         <SettingsList.Header
           headerText="Settings"
           headerStyle={styles.setting}
-          testId='settings'
         />
         <SettingsList.Item
           hasNavArrow={true}
           title="Notification settings"
           titleStyle={styles.settingItems}
-          onPress={() => Linking.openSettings()}
-          testId='openSettings'
+          id="openSetting"
+          onPress={openSetFnc}
         />
-
         <SettingsList.Header
           headerText="General"
           headerStyle={styles.general}
@@ -33,29 +45,18 @@ const Settings: React.FC<Props> = ({navigation}: Props) => {
           hasNavArrow={false}
           title="About Medstick"
           titleStyle={styles.settingItems}
-          onPress={() => navigation.navigate('aboutApp')}
-          testId='aboutApp'
+          id="about"
+          onPress={aboutFnc}
         />
         <SettingsList.Item
           hasNavArrow={false}
           title="Share with friends and family"
           titleStyle={styles.settingItems}
-          onPress={async () => {
-            try {
-              await Share.share({
-                title: 'Medstick',
-                message:
-                  'Hello you are invited to use Medstick ' +
-                  'https://play.google.com/store/apps/details?id=com.animesafar.dinterviewkit',
-                url: 'https://cdn.discordapp.com/attachments/941592669933682699/955175698568462437/vinaylogo.png',
-              });
-            } catch (error) {}
-          }}
+          id="share"
+          onPress={shareFnc}
         />
       </SettingsList>
     </View>
   );
 };
-
 export default Settings;
-
