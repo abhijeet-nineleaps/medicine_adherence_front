afdfceb7df1332a0257356e6ece671c42fe10aff
Abhijeet Chhimwal <abhijeet.chhimwal@nineleaps.com>
2022-09-15 12:26:00 +0530

    drawer navigation tested

--- src/navigation/drawerNavigator.js
+++ src/navigation/drawerNavigator.js
@@ -2,14 +2,20 @@ import React from 'react';
 import CareTaker from '../screens/caretaker/Caretaker';
 import Caretakercomp from '../screens/caretaker/CaretakerComp';
 import Patientcomp from '../screens/patient/Patient';
-import CustomHeader from '../components/customHeader';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import Settings from '../screens/Settings';
 import CameraScreen from '../screens/adherence/ClickSendImage';
 import {TouchableOpacity} from 'react-native';
+import {
+  homeIcon,
+  medIcon,
+  userIcon,
+  settingIcon,
+  camIcon,
+  manIcon,
+} from '../components/navigation/navIcons';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import IonIcon from 'react-native-vector-icons/Ionicons';
 
 const Drawer = createDrawerNavigator();
 
@@ -19,43 +25,23 @@ const DrawerNavigator = navigation => {
   };
   const homeFnc = () => {
     return (
-      <TouchableOpacity id="cam" onPress={camFnc}>
+      <TouchableOpacity id="cam" onPress={camFnc()}>
         <Icon name="camera" size={30} color="#3743ab" />
       </TouchableOpacity>
     );
   };
-  const homeIcon = () => {
-    return <Icon name="home" size={22} color="white" />;
-  };
-  const medIcon = () => {
-    return <Icon color="white" size={20} name="medkit" />;
-  };
-  const userIcon = () => {
-    <Icon size={22} color="white" name="user-md" />;
-  };
-  const settingIcon = () => {
-    return <IonIcon size={22} color="white" name="settings" />;
-  };
-  const camIcon = () => {
-    return <Icon size={20} color="white" name="camera" />;
-  };
-  const manIcon = () => {
-    return <IonIcon size={22} color="white" name="man" />;
-  };
   return (
     <Drawer.Navigator
       initialRouteName="Home"
-      screenOptions={{headerTitleStyle: {color: 'black'}}}
-      // drawerContent={<CustomHeader/>}
-      >
+      screenOptions={{headerTitleStyle: {color: 'black'}}}>
       <Drawer.Screen
         name="Home"
         options={{
           headerRightContainerStyle: {marginRight: 15},
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
-          headerRight: {homeFnc},
-          drawerIcon: {homeIcon},
+          headerRight: homeFnc(),
+          drawerIcon: homeIcon(),
         }}
         component={HomeScreen}
       />
@@ -67,7 +53,7 @@ const DrawerNavigator = navigation => {
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
           headerShown: true,
-          drawerIcon: {medIcon},
+          drawerIcon: medIcon(),
         }}
       />
       <Drawer.Screen
@@ -77,14 +63,14 @@ const DrawerNavigator = navigation => {
           title: 'My Patient',
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
-          drawerIcon: {manIcon},
+          drawerIcon: manIcon(),
         }}
       />
       <Drawer.Screen
         options={{
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
-          drawerIcon: {userIcon},
+          drawerIcon: userIcon(),
         }}
         name="My Caretaker"
         navig={this.navigation}
@@ -96,7 +82,7 @@ const DrawerNavigator = navigation => {
         options={{
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
-          drawerIcon: {settingIcon},
+          drawerIcon: settingIcon(),
         }}
       />
       <Drawer.Screen
@@ -105,7 +91,7 @@ const DrawerNavigator = navigation => {
         options={{
           drawerActiveTintColor: 'white',
           drawerInactiveTintColor: 'white',
-          drawerIcon: {camIcon},
+          drawerIcon: camIcon(),
         }}
       />
     </Drawer.Navigator>
